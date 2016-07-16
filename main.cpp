0226108e0d9f9e5f0c1794b00b549a38ca3b66f6
purukaushik <purukaushik@asu.edu>
2016-07-16 09:30:27 -0700

    refurbish string splitting

--- main.cpp
+++ main.cpp
@@ -17,24 +17,21 @@
 using namespace rapidjson;
 using namespace std;
 
-vector<string> &split(const string &s, char delim, vector<string> &elems);
 vector<string> split(const string &s, char delim);
+Document getDocumentFromFile(string fileUri);
 
-vector<string> &split(const string &s, char delim, vector<string> &elems){
+vector<string> split(const string &s, char delim){
     stringstream ss(s);
     string item;
+    vector<string> elems;
     while(getline(ss,item,delim)){
         elems.push_back(item);
     }
     return elems;
 }
 
-vector<string> split(const string &s, char delim){
-    vector<string> elems;
-    split(s,delim,elems);
-    return elems;
-}
-Document getDocumentFromFile(string fileUri);
+
+
 class Resolver : public IRemoteSchemaDocumentProvider{
 public:
     virtual const SchemaDocument* GetRemoteDocument(const Ch* uri, SizeType length);
